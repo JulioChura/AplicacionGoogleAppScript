f2d5f43a942b6901ca54edcb359be10be0795240
Marco Quispe Flores <mquispeflores@unsa.edu.pe>
2024-05-04 16:37:30 -0500

    Agregando estilo para el texto

--- texto.js
+++ texto.js
@@ -1,9 +1,24 @@
 function ModParagraphs(id) {
   var doc = DocumentApp.openById(id)
+
+  //Estilo para el texto
+  var style = {};
+  style[DocumentApp.Attribute.HORIZONTAL_ALIGNMENT] = DocumentApp.HorizontalAlignment.CENTER;
+  style[DocumentApp.Attribute.FONT_FAMILY] = 'Times New Roman';
+  style[DocumentApp.Attribute.FONT_SIZE] = 13;
+  style[DocumentApp.Attribute.BOLD] = true;
+
   var paragraphs = doc.getBody().getParagraphs();
-  paragraphs[9].setText('Año de Estudios:');
-  paragraphs[11].setText('Curso:')
-  paragraphs[13].setText('Tema:');
-  paragraphs[15].setText('Docente:')
+  paragraphs[7].setText('Curso:');
+  paragraphs[9].setText('Tema:');
+  paragraphs[11].setText('Docente:');
+  paragraphs[13].setText('Presentado por:');
+  paragraphs[16].setText('AREQUIPA - PERÚ');
+  paragraphs[17].setText('2024');
+
+  //Asignar el estilo a las lineas usadas
+  for(var i = 7; i < paragraphs.length; i++){
+    paragraphs[i].setAttributes(style);
+  }
 
 }

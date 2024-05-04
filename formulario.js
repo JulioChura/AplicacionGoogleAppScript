022d8d6307ae8a3505fa9175d632d3d432372584
Julio <jchuraaca@unsa.edu.pe>
2024-05-04 15:02:41 -0500

    funcion 1ra version que recibe la cantidad de integrantes

--- formulario.js
+++ formulario.js
@@ -1,7 +1,4 @@
-  function saludar(render){
-    var name = document.getElementById('userName').value;
-    var html = render('Hola ' + name);
-    var element = document.getElementById('saludo');
-    element.innerHTML = html;
-  
-let cantidad = doc
+function agregar() {
+    let cantidad = document.getElementById("cantidad").value;
+    console.log(cantidad);
+}

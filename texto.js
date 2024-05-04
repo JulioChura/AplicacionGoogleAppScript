d68cdd0862ccda9714919bb2cb2f5f26311768d1
Marco Quispe Flores <mquispeflores@unsa.edu.pe>
2024-05-04 11:25:25 -0500

    Modificando las secciones para datos

--- texto.js
+++ texto.js
@@ -1,14 +1,9 @@
-function ModParagraphs() {
-  var doc = DocumentApp.openById('1kqN-XWI-0KT28fMdZtJ0-2glINUSCI-ZtAQioQJIz74')
+function ModParagraphs(id) {
+  var doc = DocumentApp.openById(id)
   var paragraphs = doc.getBody().getParagraphs();
-
   paragraphs[9].setText('Año de Estudios:');
-  paragraphs[10].setText('Curso:')
-  paragraphs[11].setText('Tema:');
-  paragraphs[12].setText('Docente:')
-  paragraphs[13].setText('Estudiante:');
-  paragraphs[14].setText('Grupo:');
-  paragraphs[15].setText('Ciudad-Pais');
-  paragraphs[16].setText('Año');
+  paragraphs[11].setText('Curso:')
+  paragraphs[13].setText('Tema:');
+  paragraphs[15].setText('Docente:')
 
 }

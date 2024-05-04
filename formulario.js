a73e66603ded22ca256587160be20dee437082f0
Julio <jchuraaca@unsa.edu.pe>
2024-05-04 15:24:22 -0500

    Los elementos html se generan cuando se ingresa la canrtidad de participantes

--- formulario.js
+++ formulario.js
@@ -3,11 +3,12 @@ function agregar() {
     let maxCantidad = 3;
     if ( maxCantidad < cantidad ) {
         alert("Solo pueden agregarse 3 participantes");
-        return;
     } else {
         let body = ""
-        for ( let i = 0; cantidad < cantidad; i++ ) {
-            `
+        let box = document.getElementById("box");
+        for ( let i = 0; i < cantidad; i++ ) {
+            
+            body += `
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Nombre del integrante (Máximo 5)</label>
                 <input type="text" class="form-control" id="integrante" aria-describedby="emailHelp" name="integrante${i}>
@@ -15,9 +16,10 @@ function agregar() {
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
-            </div>
-            `
+            </div>`
         }
+    
+        box.innerHTML = body;
     }
     console.log(cantidad);
 }
